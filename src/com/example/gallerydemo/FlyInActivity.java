fd0c8885694117ed3268345a91eb7a592c4af8be
bianxh <bxh7425014@163.com>
2013-04-22 16:20:59 +0800

    修改海报在Activity退出时没有清空的情况。

--- src/com/example/gallerydemo/FlyInActivity.java
+++ src/com/example/gallerydemo/FlyInActivity.java
@@ -1,5 +1,6 @@
 package com.example.gallerydemo;
 
+import com.demo.render.DisPlayList;
 import com.example.utils.Constants;
 
 import android.app.Activity;
@@ -17,4 +18,12 @@ public class FlyInActivity extends Activity {
 		mRenderView = new RenderView(this);
 		setContentView(mRenderView);
 	}
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		DisPlayList.getInstance().clear();
+	}
+	
+	
 }
