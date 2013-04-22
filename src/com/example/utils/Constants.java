94034e6e337b88a8c737bd6f9c3ad233d178b0d6
bianxh <bxh7425014@163.com>
2013-04-22 15:10:28 +0800

    提交本地读取图片的默认路径

--- src/com/example/utils/Constants.java
+++ src/com/example/utils/Constants.java
@@ -2,4 +2,5 @@ package com.example.utils;
 
 public class Constants {
 	public static String ACTIVITY_TITLE = "activity_title";
+	public static String LOCALE_PIC_ADDR = "/sdcard/GalleryDemoTest/pic";
 }
